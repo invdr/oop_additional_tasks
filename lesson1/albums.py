bb46c883d7e21e5dc328f2b9aebcf65c04d42907
Ibrahim S <askariev@bk.ru>
2023-08-02 18:17:55 +0300

    done

--- lesson1/albums.py
+++ lesson1/albums.py
@@ -21,16 +21,18 @@
 """
 
 
-class ...:
+class Album:
 
-    def ...:
-    ...
+    def __init__(self, artist, title, tracks):
+        self.artist = artist
+        self.title = title
+        self.tracks = tracks
 
 
-album_1 = Album(...)
-
-album_2 = ...
-
+album_1 = Album('Queen',
+                'Killer Queen',
+                ['Brighton rock', 'Killer Queen', 'Tenement Funster'])
+album_2 = Album('Metallica', 'Black Album',  ['Enter Sandman', 'Sad But True', 'Holier Than Thou'])
 
 print(album_1.artist, album_1.title, len(album_1.tracks), "треков")  # Queen Killer Queen 3 треков
 print(album_2.artist, album_2.title, len(album_2.tracks), "треков")  # Metallica Black Album 3 треков
